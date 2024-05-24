9c651707df7012a9ab81fbe8c5990ea8e55aabf5
Pedro H. P. Fontana <fontana@Pedros-MacBook-Air.local>
2024-05-24 12:13:10 -0300

    spark_stream and server

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ import os
 import socket
 import pandas as pd
 from datetime import datetime
+import time
 
 def stream_csv_data(folder_path, host='localhost', port=9999):
     """
@@ -12,31 +13,39 @@ def stream_csv_data(folder_path, host='localhost', port=9999):
     host (str): Host for the socket server.
     port (int): Port for the socket server.
     """
+    # Create a TCP socket server
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the host and port
     server_socket.bind((host, port))
+
+    # Enable the server to accept connections
     server_socket.listen(1)
 
     print(f"Server listening on {host}:{port}")
 
     while True:
+        # Accept a connection from a client
         client_socket, addr = server_socket.accept()
         print(f"Connection from {addr} has been established.")
 
         for csv_file in os.listdir(folder_path):
             if csv_file.endswith(".csv"):
                 file_path = os.path.join(folder_path, csv_file)
-                df = pd.read_csv(file_path, sep='\t', infer_schema=True)
+                df = pd.read_csv(file_path, sep=',')
 
                 for _, row in df.iterrows():
-                    timestamp = row['datetime']
-                    bearing_1 = row['Bearing 1']
-                    bearing_2 = row['Bearing 2']
-                    bearing_3 = row['Bearing 3']
-                    bearing_4 = row['Bearing 4']
+                    timestamp = row[0]
+                    bearing_1 = row[1]
+                    bearing_2 = row[2]
+                    bearing_3 = row[3]
+                    bearing_4 = row[4]
 
                     # Format the data
                     data = f"timestamp: {timestamp}, bearing_1: {bearing_1}, bearing_2: {bearing_2}, bearing_3: {bearing_3}, bearing_4: {bearing_4}\n"
                     
+                    time.sleep(1)
+
                     # Send the data
                     client_socket.sendall(data.encode('utf-8'))
         
@@ -44,5 +53,5 @@ def stream_csv_data(folder_path, host='localhost', port=9999):
         print(f"Connection from {addr} has been closed.")
 
 if __name__ == "__main__":
-    folder_path = './data/2nd_test/2nd_test'  # Update this path to your folder with CSV files
+    folder_path = './data/processed/2nd_test_processed/'  # Update this path to your folder with CSV files
     stream_csv_data(folder_path)
--- app/spark_stream.py
+++ app/spark_stream.py
@@ -1,3 +1,24 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
 
-spark = SparkSession.builder.master("local[*]").getOrCreate()
+# Create a SparkSession
+spark = SparkSession.builder.appName("Bearing Data Processing").getOrCreate()
+
+# Read JSON data from socket with the defined schema
+line = spark \
+    .readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
+
+# Start the streaming query to process the parsed data from the socket
+query = line \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
+
+# Wait for the termination of the query
+query.awaitTermination()
